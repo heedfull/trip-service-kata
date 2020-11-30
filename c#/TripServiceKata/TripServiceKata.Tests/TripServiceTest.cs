33467b9afa4f2c1d8b5a5fd2659e417661156ee3
Edwin Goddard <edwin.goddard@fintrax.com>
2020-11-30 07:23:24 +0000

    TB-3432 test guest scenario

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -1,6 +1,34 @@
-﻿namespace TripServiceKata.Tests
+﻿using Xunit;
+using TripServiceKata.Trip;
+using TripServiceKata.Exception;
+
+namespace TripServiceKata.Tests
 {
     public class TripServiceTest
     {
+        private readonly User.User GUEST;
+        private readonly User.User UNUSED_USER;
+
+        [Fact]
+        public void ShouldThrowAnExceptionWhenUserIsNotLoggedIn()
+        {
+            var sut = new TestableTripService();
+
+            sut.LoggedInUser = GUEST;
+
+            Assert.Throws<UserNotLoggedInException>(() => sut.GetTripsByUser(UNUSED_USER));
+        }
+
+
+        public class TestableTripService : TripService
+        {
+            public User.User LoggedInUser;
+
+            public override User.User GetLoggedInUser()
+            {
+                return LoggedInUser;
+            }
+
+        }
     }
 }
