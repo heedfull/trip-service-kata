c0e2b73dab680509b213495ccbab545904e23189
Edwin Goddard <edwin.goddard@fintrax.com>
2020-12-02 09:42:41 +0000

    move userbuild to it's own class and staticly import it

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -2,7 +2,8 @@
 using TripServiceKata.Trip;
 using TripServiceKata.Exception;
 using System.Collections.Generic;
-using System.Linq;
+using static TripServiceKata.Tests.UserBuilder;
+
 
 namespace TripServiceKata.Tests
 {
@@ -37,10 +38,10 @@ namespace TripServiceKata.Tests
         [Fact]
         public void ShouldNotReturnAnyTripsWhenUsersAreNotFriends()
         {
-            var friend = UserBuilder.AUser()
-            .FriendsWith(ANOTHER_USER)
-            .WithTrips(TO_BRAZIL)
-            .Build();
+            var friend = AUser()
+                .FriendsWith(ANOTHER_USER)
+                .WithTrips(TO_BRAZIL)
+                .Build();
 
             var friendTrips = tripService.GetTripsByUser(friend);
 
@@ -50,7 +51,7 @@ namespace TripServiceKata.Tests
         [Fact]
         public void ShouldReturnTripsWhenUsersAreFriends()
         {
-            var friend = UserBuilder.AUser()
+            var friend = AUser()
                 .FriendsWith(ANOTHER_USER, LoggedInUser)
                 .WithTrips(TO_BRAZIL, TO_LONDON)
                 .Build();
@@ -78,53 +79,6 @@ namespace TripServiceKata.Tests
             {
                 return user.Trips();
             }
-        }
-    }
-
-    public class UserBuilder
-    {
-        private List<User.User> Friends = new List<User.User>();
-        private List<Trip.Trip> Trips = new List<Trip.Trip>();
-
-        internal static UserBuilder AUser()
-        {
-            return new UserBuilder();
-        }
-
-        internal User.User Build()
-        {
-            var user = new User.User();
-            AddTripsTo(user);
-            AddFriendsTo(user);
-            return user;
-        }
-
-        private void AddFriendsTo(User.User user)
-        {
-            foreach (var friend in Friends)
-            {
-                user.AddFriend(friend);
-            }
-        }
-
-        private void AddTripsTo(User.User user)
-        {
-            foreach (var trip in Trips)
-            {
-                user.AddTrip(trip);
-            }
-        }
-
-        internal UserBuilder FriendsWith(params User.User[] friends)
-        {
-            Friends = friends.ToList();
-            return this;
-        }
-
-        internal UserBuilder WithTrips(params Trip.Trip[] trips)
-        {
-            Trips = trips.ToList();
-            return this;
         }
     }
 }
---
+++ c#/TripServiceKata/TripServiceKata.Tests/UserBuilder.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TripServiceKata.Tests
+{
+    public class UserBuilder
+    {
+        private List<User.User> Friends = new List<User.User>();
+        private List<Trip.Trip> Trips = new List<Trip.Trip>();
+
+        internal static UserBuilder AUser()
+        {
+            return new UserBuilder();
+        }
+
+        internal User.User Build()
+        {
+            var user = new User.User();
+            AddTripsTo(user);
+            AddFriendsTo(user);
+            return user;
+        }
+
+        private void AddFriendsTo(User.User user)
+        {
+            foreach (var friend in Friends)
+            {
+                user.AddFriend(friend);
+            }
+        }
+
+        private void AddTripsTo(User.User user)
+        {
+            foreach (var trip in Trips)
+            {
+                user.AddTrip(trip);
+            }
+        }
+
+        internal UserBuilder FriendsWith(params User.User[] friends)
+        {
+            Friends = friends.ToList();
+            return this;
+        }
+
+        internal UserBuilder WithTrips(params Trip.Trip[] trips)
+        {
+            Trips = trips.ToList();
+            return this;
+        }
+    }
+}
